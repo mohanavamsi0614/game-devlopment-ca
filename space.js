2779a5d7e1c481127625d01a382dd88ffffe7bc3
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2023-12-13 18:38:44 +0530

    added

--- space.js
+++ space.js
@@ -1,51 +1,56 @@
-const dino=document.getElementById("dinoship")
-const space=document.getElementById("space")
+const dino = document.getElementById("dinoship");
+const space = document.getElementById("space");
 // const right=document.getElementById("right")
 // const left=document.getElementById("left")
 // const dleft=document.getElementById("dleft")
 // const dright=document.getElementById("dright")
 // pos=["",right,left,dleft,dright]
-const random=()=>{
-    return Math.ceil(Math.random()*4)
-}
+const random = () => {
+  return Math.ceil(Math.random() * 4);
+};
 // function bullet(){
-    
+let i=0
 // }
-function ship(){
-    // console.log(pos[random()])
-    const img=document.createElement("img")
-    img.src=`./assets/ship${random()}.png`
-    img.classList.add("villain")
-    img.style.animation=`enemey${random()} 2.3s linear`
-    space.append(img)
-    img.onclick=()=>{
+function ship() {
+  // console.log(pos[random()])
+  const img = document.createElement("img");
+  img.src = `./assets/ship${random()}.png`;
+  img.id=i
+  img.classList.add("villain");
+  img.style.animation = `enemey${random()} 2.3s linear`;
+  space.appendChild(img);
+  console.log(img)
+  img.onclick = () => {
+    space.removeChild(img);
+    i++
+    ship()
+  };
+  check()
+//   setInterval(()=>{  check(img)
+//   }, 10);
+function check(){
+    // const img=document.querySelector(".villain")
+    if (img.getBoundingClientRect().top>=dino.getBoundingClientRect().top){
         space.removeChild(img)
+        i++
+        ship()
+        alert("hi")
         // ship()
     }
-    setInterval(check,0)
-    // del(img)
 }
-setInterval(ship,3000)
-console.log(dino.getbo)
-function check() {
-    let i=document.querySelector(".villain")
-    i.style.border="1px solid red"
-    console.log(i.getBoundingClientRect())
-        if(i.getBoundingClientRect().top>=dino.getBoundingClientRect().top){
-            alert("done")
-
-            space.removeChild(i)
-            alert("done")
-
-            console.log("done!",i.getBoundingClientRect())
-            // ship()
-        };
-    
+setInterval(check,30)
 }
-ship()
+  // del(img)
+// }
+// setInterval(ship, 3000);
+
+// console.log(dino.getbo);
+
+
+ship();
 // addEventListener("click",()=>{
-// }) 
+// })
 // 463.6484375
 // top
-// : 
+// :
 // 391.3671875
